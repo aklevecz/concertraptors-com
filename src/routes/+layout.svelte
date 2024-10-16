4fe833813e88d393ad3721549d804299dc984e48
ariel klevecz <ariel@yaytso.art>
2024-10-16 08:53:50 -0700

    2

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,21 @@
+<script>
+    let {children} = $props()
+</script>
+
+{@render children()}
+
+<style>
+    :global(html) {
+        --primary-color: #f9f9f9b4;
+        --secondary-color: #33aaff;
+    }
+    :global(body) {
+        margin: 0;
+        font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen', 'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue', sans-serif;
+        background-color: var(--primary-color);
+        color: var(--secondary-color)
+    }
+    :global(a) {
+        color: var(--secondary-color);
+    }
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,45 @@
+<svelte:head>
+    <title>CONCERT RAPTORS</title>
+    <meta name="description" content="Concert Raptors est. 2015 - Music communities around the world with people connecting through the love for music they share." />
+    <meta name="keywords" content="music, concert, community, raptors" />
+    <meta property="og:title" content="CONCERT RAPTORS" />
+    <meta property="og:description" content="Music communities around the world with people connecting through the love for music they share." />
+    <meta property="og:image" content="https://concertraptors.com/ogimage.png" />
+    <meta property="og:url" content="https://concertraptors.com" />
+    <meta name="twitter:card" content="summary_large_image" />
+</svelte:head>
+<h2>CONCERT RAPTORS</h2>
+<div style="text-align: center;font-size:.65rem;">est. 2015</div>
+<img class="raptor" src="raptor-svg.svg" alt="raptor" />
+<ul>
+  <li><a href="https://www.facebook.com/groups/concertraptor">SF Community</a></li>
+  <li><a href="https://www.facebook.com/groups/1220577598063565/">LA Community</a></li>
+  <li><a href="https://www.facebook.com/groups/nycconcertraptors">NYC Community</a></li>
+  <li><a href="https://www.facebook.com/groups/concertraptorsdenver/">Denver Community</a></li>
+  <li><a href="https://www.facebook.com/groups/364578720993220">Berlin Community</a></li>
+  <li><a href="hhttps://www.facebook.com/groups/londonconcertraptors/">London Community</a></li>
+  <li><a href="https://www.facebook.com/groups/1107707530207340">SD Community</a></li>
+  <li><a href="https://www.facebook.com/groups/concertraptorspnw">PNW Community</a></li>
+  <li><a href="https://www.facebook.com/groups/515407205608480">Portland Community</a></li>
+  <li><a href="https://www.facebook.com/groups/657802501799891/">Austin Community</a></li>
+  <li><a href="https://www.facebook.com/groups/986315544842598/">ATL Community</a></li>
+</ul>
+
+<style>
+  h2 {
+    text-align: center;
+    margin-bottom: 0;
+  }
+  .raptor {
+    max-width: 100px;
+    margin: auto;
+    display: block;
+    padding: 1rem;
+  }
+  ul {
+    list-style-type: none;
+  }
+  li {
+    padding: 0.5rem;
+  }
+</style>
